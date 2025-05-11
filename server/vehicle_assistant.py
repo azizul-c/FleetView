4b8263f0348d3a23fd1162c2c91e2b61d2d9c4cb
Azizul Chowdhury <a48chowd@uwaterloo.ca>
2025-05-11 17:51:18 -0400

    Fix CORS issue

--- server/vehicle_assistant.py
+++ server/vehicle_assistant.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000", "https://fleetview.vercel.app/"])
+CORS(app, origins=["http://localhost:3000", "https://fleetview.vercel.app"])
 
 load_dotenv()
 
